d4d4544de01fba2f4a70b1658dfbf47a54130496
Preisler25 <preisler.andras@gmail.com>
2023-12-03 15:07:38 +0100

    project init

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,11 @@
 // src/index.ts
 import express from 'express';
-import { Pool } from 'pg';
 
 const app = express();
 const port = 3000;
 
-const pool = new Pool({
-  user: 'postgres',
-  host: 'db', // This is the name of the PostgreSQL service defined in the docker-compose.yml
-  database: 'postgres',
-  password: 'password',
-  port: 5432,
-});
-
 app.get('/', async (req, res) => {
-  try {
-    const { rows } = await pool.query('SELECT $1::text as message', ['Hello World']);
-    res.json({ message: rows[0].message });
-  } catch (error) {
-    console.error('Error executing query', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+    res.send('Hello World!');
 });
 
 app.listen(port, () => {
